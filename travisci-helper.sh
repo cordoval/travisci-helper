ea144a9481c62e1dec120057f534e2e53b0994a1
Julien Bianchi <julien.bianchi@pmsipilot.com>
2012-07-05 10:12:43 +0200

    Improved scripts

--- travisci-helper.sh
+++ travisci-helper.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 sudo sh -e .travis/scripts/apt-get.sh
-sudo sh -e .travis/scripts/apache2-vhost.sh "travisci-behat.local"
+sudo sh -e .travis/scripts/apache2-vhost.sh "[myapp].local"
 sudo sh -e .travis/scripts/apache2-configure.sh
 sh -e .travis/scripts/mysql.sh
 sh -e .travis/scripts/sahi-install.sh
 
-sh -e .travis/scripts/sf2-configure.sh
-sh -e .travis/scripts/behat-configure.sh "travisci-behat.local" "phantomjs"
+sh -e .travis/scripts/sf2-configure.sh "[mydatabase]"
+sh -e .travis/scripts/behat-configure.sh "[myapp].local" "[mybrowser]"
 sh -e .travis/scripts/sahi-configure.sh
 sh -e .travis/scripts/sf2-init.sh
 sh -e .travis/scripts/sahi-start.sh
 
+phpunit
 php behat.phar
